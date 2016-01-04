9a2d088ad06a0dbe3b6e4e413d8f80ebabcf13ac
Marco Cardacci <marco.cardacci@gmail.com>
2016-01-04 00:01:43 -0500

    api outdated, find workaround

---
+++ deliciousrec.py
@@ -0,0 +1,14 @@
+from pydelicious import get_popular,get_userposts,get_urlposts
+import time
+
+def initializeUserDict(tag, count=5):
+  user_dict={}
+  #get the top count' popular posts
+  for p1 in get_popular(tag=tag)[0:count]:
+  	# find all users who posted this
+  	for p2 in get_urlposts(p1['href']):
+  	  user=p2['user']
+  	  user_dict[user]={}
+  return user_dict
+
+initializeUserDict('science')
