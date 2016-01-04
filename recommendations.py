5b6510d877308d27a6d48728472bea227eba573a
Marco Cardacci <marco.cardacci@gmail.com>
2016-01-03 20:13:52 -0500

    adds merge conflict

--- recommendations.py
+++ recommendations.py
@@ -30,13 +30,12 @@ def sim_distance(prefs, person1, person2):
     for item in prefs[person1]:
         if item in prefs[person2]:
             si [item] = 1
-    print(si)
-
+            
     #if they have no ratings in common return 0
     if len(si) == 0: return 0
 
     #Add up the squares of all the differences
-    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
-                            for item in si])
+    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in si])
     return 1/(1+sqrt(sum_of_squares))
     
+# PEARSON CORRELATION SCORE
